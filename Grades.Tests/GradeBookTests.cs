f0cd7725fc24af2c0dbe0bef7cb7f2be3434ca4d
NoelleDL <noelle.legrain@gmail.com>
2018-06-27 14:36:36 +0100

    Added test file and test to check highest grade

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,33 @@
+using Grades; 
+using System;
+using Xunit;
+
+namespace Grades.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.Equal(90, result.HighestGrade); 
+        }
+
+        [Fact]
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(90);
+            book.AddGrade(10);
+
+            GradeStatistics result = book.ComputeStatistics();
+
+            Assert.Equal(10, result.LowestGrade); 
+        }
+    }
+}
