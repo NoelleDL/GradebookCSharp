9df09752d45e47740374255446a5a4ddc57ed2c2
NoelleDL <noelle.legrain@gmail.com>
2018-06-27 09:58:48 +0100

    Added class and method to add grades to gradebook

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>(); 
+    }
+}
