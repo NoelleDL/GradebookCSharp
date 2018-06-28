5a6dc561e3a91964d1cad6ec818df321fd4edb95
NoelleDL <noelle.legrain@gmail.com>
2018-06-28 10:31:49 +0100

    Added Name property with getter and setter

--- Grades/Program.cs
+++ Grades/Program.cs
@@ -6,12 +6,14 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            GradeBook book = new GradeBook(); 
+            GradeBook book = new GradeBook();
+            book.Name = "Noelle"; 
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(79); 
 
-            GradeStatistics stats = book.ComputeStatistics(); 
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name); 
             WriteResult("Average", stats.AverageGrade);
             WriteResult("Highest", (int)stats.HighestGrade);
             WriteResult("Lowest", stats.LowestGrade);
